2adb3044677efbb3f0c9360ea4774adbb30f4945
imdiegui1 <da.naranjo10@uniandes.edu.co>
2023-09-01 16:45:57 -0500

    example how to get values from db

---
+++ recuperar.py
@@ -0,0 +1,32 @@
+from src.cancion import Cancion
+from src.interprete import Interprete
+from src.album import Album, Medio
+from src.declarative_base import Session, engine, Base
+
+
+if __name__ == '__main__':
+  session = Session()
+  canciones = session.query(Cancion).all()
+
+  print('Las canciones almacenadas son:')
+  for cancion in canciones:
+      print("Titulo: " + cancion.titulo + " (00:" +
+            str(cancion.minutos) + ":" +
+            str(cancion.segundos) + ")")
+
+      print("Intérpretes")
+      for interprete in cancion.interpretes:
+          print(" - " + interprete.nombre)
+
+      for album in cancion.albumes:
+          print(" -- Presente en el album: " + album.titulo)
+
+      print("")
+
+
+  print('Los álbumes almacenados en discos son:')
+  albumes = session.query(Album).filter(Album.medio == Medio.DISCO).all()
+  for album in albumes:
+      print("Album: " + album.titulo)
+
+  session.close()
